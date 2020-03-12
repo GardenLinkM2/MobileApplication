900cbc72604d76d29bf59e437f028905052301c4
NicolasL M2 <master_nicolasl@local.dev>
2020-03-12 23:17:45 +0000

    (Attention peut-etre test chacun chez soit pour éviter les comportement chimique) Nl connection persitence

---
+++ app/src/main/java/com/gardenlink_mobile/utils/PreferenceUtils.java
@@ -0,0 +1,42 @@
+package com.gardenlink_mobile.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+
+public class PreferenceUtils {
+
+    private static String KEY_EMAIL = "Email";
+    private static String KEY_PASSWORD = "Password";
+
+    public PreferenceUtils(){
+
+    }
+
+    public static boolean saveEmail(String email, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString(KEY_EMAIL, email);
+        prefsEditor.apply();
+        return true;
+    }
+
+    public static String getEmail(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(KEY_EMAIL, null);
+    }
+
+    public static boolean savePassword(String password, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString(KEY_PASSWORD, password);
+        prefsEditor.apply();
+        return true;
+    }
+
+    public static String getPassword(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(KEY_PASSWORD, null);
+    }
+}
